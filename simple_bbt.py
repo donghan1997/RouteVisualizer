099e6ccdff1a828d003cc4f9cff54ac4b4f8550d
donghan <donghan@ufl.edu>
2025-06-28 00:38:33 -0400

    change env

--- simple_bbt.py
+++ simple_bbt.py
@@ -75,7 +75,7 @@ def analyze_tree(log_file, save_pic=False, output_folder=None, show_tree=True):
         st.info(f"📁 Saving results to: {output_folder}")
     
     # Build the command
-    cmd = ["/home/adminuser/venv/bin/python3", bbt_script, log_file]
+    cmd = ["/home/adminuser/venv/bin/python", bbt_script, log_file]
     
     if save_pic:
         cmd.extend(["--save-intermediates", "--output-dir", output_folder])
